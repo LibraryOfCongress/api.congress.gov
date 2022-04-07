0149e678b9cae24f4725666e2338ba8b5989fc29
Jon Sgaggero <jsgaggero@loc.gov>
2022-04-07 16:38:28 -0400

    Add save output and some refactoring

--- java/src/loc/gov/Utils.java
+++ java/src/loc/gov/Utils.java
@@ -1,6 +1,9 @@
 package loc.gov;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -18,22 +21,64 @@ public class Utils {
      */
     public static void makeRequest(String url) {
 
-        printMessage("Making call to " + url);
+        outputMessage("Making call to " + url);
 
+        // Build the client and request
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(appendParameters(url)))
                 .build();
 
+        // Make the call & get a response
         HttpResponse<String> response = null;
         try {
-            response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (IOException | InterruptedException ex) {
-            printMessage(ex.getMessage());
+            outputMessageAndBail(ex.getMessage());
+        }
+        
+        // If our response is null, something wasn't right
+        if (response == null || response.body() == null) {
+            outputMessageAndBail("Something went wrong, we didn't get a response from " + url);
         }
 
-        printMessage(response.body());
+        // Save the output to a file if our config says to do so
+        if (Config.getStoreResponse().equals("true")) {
+            saveOutputToNamedTextFile(response, url);
+        } else {
+            // Otherwise, print it to the screen
+            outputMessage(response.body());
+        }
+    }
+
+    /**
+     * saveOutput
+     * 
+     * Save the output of the call to url in a text file named for the route.
+     * 
+     * @param output The HttpResponse to save
+     * @param url The url that was called that provided the response
+     */
+    private static void saveOutputToNamedTextFile(HttpResponse<String> output, String url) {
+        String filename = url.replace(Config.getRootUrl() + "/", "").replace("/", "_") + ".txt";
+
+        // Store our default output
+        PrintStream console = System.out;
+
+        // Set up our file output
+        PrintStream fileOut = null;
+        try {
+            fileOut = new PrintStream(filename);
+        } catch (FileNotFoundException ex) {
+            outputMessageAndBail("Unable to create the output file " + filename);
+        }
+        assert fileOut != null;
+        System.setOut(fileOut);
+
+        outputMessage(output.body());
+
+        // Restore the default output
+        System.setOut(console);
     }
 
     /**
@@ -42,33 +87,32 @@ public class Utils {
      *
      * @param url The URL to append the authkey and format URL parameters to
      *
-     * @return String
+     * @return String The url with the appended auth and format parameters
      */
     private static String appendParameters(String url) {
         return url + "?authkey=" + Config.getAuthKey() + "&format=" + Config.getResponseFormat();
     }
 
     /**
-     * printMessage
+     * outputMessage
      *
      * Display a message on the console
      *
-     * @param message
+     * @param message The message to output to System.out
      */
-    public static void printMessage(String message) {
+    public static void outputMessage(String message) {
         System.out.println(message);
-
     }
 
     /**
-     * printMessageAndBail
+     * outputMessageAndBail
      *
      * Display a message on the console and exit
      *
-     * @param message
+     * @param message The message to output to System.out
      */
-    public static void printMessageAndBail(String message) {
-        printMessage(message);
+    public static void outputMessageAndBail(String message) {
+        outputMessage(message);
         System.exit(1);
     }
 }
