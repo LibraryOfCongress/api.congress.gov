61491cb6b20b99730f2e8e36f77a986412c009b9
Jon Sgaggero <jsgaggero@loc.gov>
2022-04-07 16:22:52 -0400

    Renaming

--- java/src/loc/gov/Bill.java
+++ java/src/loc/gov/Bill.java
@@ -2,8 +2,8 @@ package loc.gov;
 
 public class Bill {
 
-    private static final String HR       = Config.getBillHr();
-    private static final String NUM      = Config.getBillNum();
+    private static final String CHAMBER  = Config.getBillChamber();
+    private static final String NUMBER   = Config.getBillNumber();
     private static final String URL      = Config.getBillUrl();
     private static final String CONGRESS = Config.getBillCongress();
 
@@ -15,152 +15,151 @@ public class Bill {
      * Runs all the bill endpoints against the API
      *
      */
-    public static void runBillMethods() {
-
-        getBills();
-        getBillsCongress();
-        getBillsListType();
-        getBillDetail();
-        getBillActions();
-        getBillAmendments();
-        getBillCommittees();
-        getBillCosponsors();
-        getBillRelatedBills();
-        getBillSubjects();
-        getBillSummaries();
-        getBillTexts();
-        getBillTitles();
+    public static void runAllBillMethods() {
+        getAllBills();
+        getAllBillsForCongress();
+        getAllBillsForCongressionalChamber();
+        getABillsDetails();
+        getABillsActions();
+        getABillsAmendments();
+        getABillsCommittees();
+        getABillsCosponsors();
+        getABillsRelatedBills();
+        getABillsSubjects();
+        getABillsSummaries();
+        getABillsTexts();
+        getABillsTitles();
     }
 
     /**
-     * getBill
+     * getAllBills
      *
      * Gets all bills
      *
      */
-    public static void getBills() {
+    public static void getAllBills() {
         String url = String.format("%s/%s", ROOT_URL, URL);
         Utils.makeRequest(url);
     }
 
     /**
-     * getBillsCongress
+     * getAllBillsForCongress
      *
      * Gets all bills for the specified congress
      */
-    public static void getBillsCongress() {
+    public static void getAllBillsForCongress() {
         String url = String.format("%s/%s/%s", ROOT_URL, URL, CONGRESS);
         Utils.makeRequest(url);
     }
 
     /**
-     * getBillsListType
+     * getAllBillsForCongressionalChamber
      *
      * Gets all bills for the specified congress and chamber
      *
      */
-    public static void getBillsListType() {
-        String url = String.format("%s/%s/%s/%s", ROOT_URL, URL, CONGRESS, HR);
+    public static void getAllBillsForCongressionalChamber() {
+        String url = String.format("%s/%s/%s/%s", ROOT_URL, URL, CONGRESS, CHAMBER);
         Utils.makeRequest(url);
     }
 
     /**
-     * getBillDetail
+     * getABillsDetails
      *
      * Gets the detail for the specified bill
      */
-    public static void getBillDetail() {
-        String url = String.format("%s/%s/%s/%s/%s", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsDetails() {
+        String url = String.format("%s/%s/%s/%s/%s", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
         Utils.makeRequest(url);
     }
 
     /**
-     * getBillActions
+     * getABillsActions
      *
      * Gets the actions for the specified bill
      */
-    public static void getBillActions() {
-        String url = String.format("%s/%s/%s/%s/%s/actions", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsActions() {
+        String url = String.format("%s/%s/%s/%s/%s/actions", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
         Utils.makeRequest(url);
     }
 
     /**
-     * getBillAmendments
+     * getABillsAmendments
      *
      * Gets the amendments for the specified bill
      */
-    public static void getBillAmendments() {
-        String url = String.format("%s/%s/%s/%s/%s/amendments", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsAmendments() {
+        String url = String.format("%s/%s/%s/%s/%s/amendments", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
         Utils.makeRequest(url);
     }
 
     /**
-     * getBillCommittees
+     * getABillsCommittees
      *
      * Gets the committees for the specified bill
      */
-    public static void getBillCommittees() {
-        String url = String.format("%s/%s/%s/%s/%s/committees", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsCommittees() {
+        String url = String.format("%s/%s/%s/%s/%s/committees", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
         Utils.makeRequest(url);
     }
 
     /**
-     * getBillCosponsors
+     * getABillsCosponsors
      *
      * Gets the cosponsors for the specified bill
      */
-    public static void getBillCosponsors() {
-        String url = String.format("%s/%s/%s/%s/%s/cosponsors", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsCosponsors() {
+        String url = String.format("%s/%s/%s/%s/%s/cosponsors", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
         Utils.makeRequest(url);
     }
 
     /**
-     * getBillRelatedBills
+     * getABillsRelatedBills
      *
      * Gets the related bills for the specified bill
      */
-    public static void getBillRelatedBills() {
-        String url = String.format("%s/%s/%s/%s/%s/relatedbills", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsRelatedBills() {
+        String url = String.format("%s/%s/%s/%s/%s/relatedbills", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
         Utils.makeRequest(url);
     }
 
     /**
-     * getBillSubjects
+     * getABillsSubjects
      *
      * Gets the subjects for the specified bill
      */
-    public static void getBillSubjects() {
-        String url = String.format("%s/%s/%s/%s/%s/subjects", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsSubjects() {
+        String url = String.format("%s/%s/%s/%s/%s/subjects", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
        Utils.makeRequest(url);
     }
 
     /**
-     * getBillSummaries
+     * getABillsSummaries
      *
      * Gets the summaries for the specified bill
      */
-    public static void getBillSummaries() {
-        String url = String.format("%s/%s/%s/%s/%s/summaries", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsSummaries() {
+        String url = String.format("%s/%s/%s/%s/%s/summaries", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
        Utils.makeRequest(url);
     }
 
     /**
-     * getBillTexts
+     * getABillsTexts
      *
      * Gets the texts for the specified bill
      */
-    public static void getBillTexts() {
-        String url = String.format("%s/%s/%s/%s/%s/text", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsTexts() {
+        String url = String.format("%s/%s/%s/%s/%s/text", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
        Utils.makeRequest(url);
     }
 
     /**
-     * getBillTitles
+     * getABillsTitles
      *
      * Gets the titles for the specified bill
      */
-    public static void getBillTitles() {
-        String url = String.format("%s/%s/%s/%s/%s/titles", ROOT_URL, URL, CONGRESS, HR, NUM);
+    public static void getABillsTitles() {
+        String url = String.format("%s/%s/%s/%s/%s/titles", ROOT_URL, URL, CONGRESS, CHAMBER, NUMBER);
        Utils.makeRequest(url);
     }
 }
