e9d896f7d96392363fd26204294a9d5a2ac6d3ff
Jon Sgaggero <jsgaggero@loc.gov>
2022-04-07 12:31:57 -0400

    Add Main

---
+++ java/src/loc/gov/Main.java
@@ -0,0 +1,17 @@
+package loc.gov;
+
+import java.io.IOException;
+
+public class Main {
+
+    /**
+     *
+     * @param args
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Bill.run_bill_methods();
+    }
+}
