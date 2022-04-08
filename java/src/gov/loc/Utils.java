692ec264873bf1a8fccc34a4a2f03fbd2103c53b
Jon Sgaggero <jsgaggero@loc.gov>
2022-04-08 12:03:59 -0400

    add overloaded method for output message

--- java/src/gov/loc/Utils.java
+++ java/src/gov/loc/Utils.java
@@ -128,6 +128,23 @@ public class Utils {
     }
 
     /**
+     * outputMessage
+     *
+     * overloaded
+     *
+     * Display a message on the console (with optional newline)
+     *
+     * @param message    The message to output to System.out
+     * @param addNewline adds a newline at the end of the message if true
+     */
+    public static void outputMessage(String message, boolean addNewline) {
+        String newline = "";
+        if (addNewline)
+            newline = System.lineSeparator();
+        outputMessage(message + newline);
+    }
+
+    /**
      * outputMessageAndBail
      *
      * Display a message on the console and exit
